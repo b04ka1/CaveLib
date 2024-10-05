158cc84966a1b7dd99e6e57262fd7f278a63bc20
b04ka <daniksla9@gmail.com>
2024-10-05 22:53:20 +0300

    Fixed Trial Chamber generating in caves

--- src/main/java/com/b04ka/cavelib/misc/CLTags.java
+++ src/main/java/com/b04ka/cavelib/misc/CLTags.java
@@ -7,7 +7,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 
 public class CLTags {
-     public static final TagKey<Biome> HAS_NO_ANCIENT_CITIES_IN = registerBiomeTag("has_no_ancient_cities_in");
+     public static final TagKey<Biome> HAS_NO_ANCIENT_CITIES_AND_TRIAL_CHAMBERS_IN = registerBiomeTag("has_no_ancient_cities_and_trial_chambers_in");
 
     private static TagKey<Biome> registerBiomeTag(String name) {
         return TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(CaveLib.MODID, name));
--- src/main/java/com/b04ka/cavelib/mixin/JigsawStructureMixin.java
+++ src/main/java/com/b04ka/cavelib/mixin/JigsawStructureMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +22,8 @@ public class JigsawStructureMixin {
 
     @Shadow @Final private Optional<ResourceLocation> startJigsawName;
 
+    @Shadow @Final private Holder<StructureTemplatePool> startPool;
+
     @Inject(
             method = {"Lnet/minecraft/world/level/levelgen/structure/structures/JigsawStructure;findGenerationPoint(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;)Ljava/util/Optional;"},
             remap = true,
@@ -28,12 +31,12 @@ public class JigsawStructureMixin {
             at = @At(value = "HEAD")
     )
     private void cl_findGenerationPoint(Structure.GenerationContext context, CallbackInfoReturnable<Optional<Structure.GenerationStub>> cir) {
-        if(this.startJigsawName.isPresent() && this.startJigsawName.get().toString().equals("minecraft:city_anchor")){ // limit to only ancient cities
+        if((this.startJigsawName.isPresent() && this.startJigsawName.get().toString().equals("minecraft:city_anchor")) || this.startPool.is(ResourceLocation.withDefaultNamespace("trial_chambers/chamber/end"))){// limit to only ancient cities and trial chambers
             int i = context.chunkPos().getBlockX(9);
             int j = context.chunkPos().getBlockZ(9);
 
             for (Holder<Biome> holder : ACMath.getBiomesWithinAtY(context.biomeSource(), i, context.chunkGenerator().getSeaLevel() - 80, j, 80, context.randomState().sampler())) {
-                if (holder.is(CLTags.HAS_NO_ANCIENT_CITIES_IN)) {
+                if (holder.is(CLTags.HAS_NO_ANCIENT_CITIES_AND_TRIAL_CHAMBERS_IN)) {
                     cir.setReturnValue(Optional.empty());
                 }
             }
